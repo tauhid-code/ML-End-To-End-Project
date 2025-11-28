881521f7227b8a5899f909122450dc1d213b4079
tauhid-code <tauhidurrehman7@gmail.com>
2025-11-28 22:52:04 +0530

    Pushed files

---
+++ src/utils.py
@@ -0,0 +1,59 @@
+import os
+import sys
+
+import numpy as np
+import pandas as pd
+import dill
+
+from sklearn.metrics import r2_score
+
+from src.exception import CustomException
+
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            dill.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    """
+    Train each model in `models` dict and return
+    a report: {model_name: test_r2_score}
+    """
+    try:
+        report = {}   
+
+        for model_name, model in models.items():
+            
+            model.fit(X_train, y_train)
+
+            
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            
+            report[model_name] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+def load_objects(file_path):
+    try:
+        with open(file_path,"rb") as file_obj:
+            return dill.load(file_obj)
+        
+    except Exception as e:
+        raise CustomException(e,sys)    
